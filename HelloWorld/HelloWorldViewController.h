686c65a4995558e5c6a6d6a245bafcbfa2684838
naush <lihsuan@8thlight.com>
2011-06-16 22:04:43 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Li-Hsuan Lung on 6/16/11.
+//  Copyright 2011 8th Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+    
+}
+
+@end
