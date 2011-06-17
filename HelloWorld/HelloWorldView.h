1cc41b1d38baacc665bac79ace462c869f3e0ae9
naush <lihsuan@8thlight.com>
2011-06-17 11:25:09 -0500

    first commit

---
+++ HelloWorld/HelloWorldView.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldView.h
+//  HelloWorld
+//
+//  Created by Li-Hsuan Lung on 6/16/11.
+//  Copyright 2011 8th Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HelloWorldView : UIView {
+    
+}
+
+@end
---
+++ HelloWorld/HelloWorldView.m
@@ -0,0 +1,37 @@
+//
+//  HelloWorldView.m
+//  HelloWorld
+//
+//  Created by Li-Hsuan Lung on 6/16/11.
+//  Copyright 2011 8th Light. All rights reserved.
+//
+
+#import "HelloWorldView.h"
+
+
+@implementation HelloWorldView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
